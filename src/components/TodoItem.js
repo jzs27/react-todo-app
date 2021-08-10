35408d46b29763070f5a32fa95d9c4c0471213f4
jzs27 <71947227+jzs27@users.noreply.github.com>
2021-08-10 16:54:06 -0500

    Updating the state using the setState() method

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,12 @@ class TodoItem extends React.Component {
   render() {
     return (
         <li>
-          <input type="checkbox" /> {this.props.todo.title}
+          {this.props.todo.title} 
+          <input
+  type="checkbox"
+  checked={this.props.todo.completed}
+  onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+/>
         </li>
       )
   }
