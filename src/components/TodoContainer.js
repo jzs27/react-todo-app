b70fe6d3b258a44310f2ca8a4dbaa00d4ebd3e5e
jzs27 <71947227+jzs27@users.noreply.github.com>
2021-08-11 07:29:06 -0500

    Implemented React Lifecycle Methods

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -4,28 +4,9 @@ import Header from "./Header"
 import InputTodo from "./InputTodo"
 import { v4 as uuidv4 } from "uuid";
 class TodoContainer extends React.Component {
-    state = {
-        todos:[
-            {
-                id:uuidv4(),
-                title:"Setup development environment.",
-                completed: true
-
-            },
-            {
-                id:uuidv4(),
-                title:"Develop website and add content.",
-                completed:false
-            },
-            {
-                id:uuidv4(),
-                title:"Deploy to live server.",
-                completed:false
-            }
-        ]
-
-    };
-
+  state = {
+    todos: [],
+  }
     handleChange = id => {
         this.setState(prevState => ({
             todos: prevState.todos.map(todo => {
@@ -39,6 +20,27 @@ class TodoContainer extends React.Component {
             }),
           }))
       };
+
+      componentDidUpdate(prevProps, prevState) {
+        if(prevState.todos !== this.state.todos) {
+          const temp = JSON.stringify(this.state.todos)
+          localStorage.setItem("todos", temp)
+        }
+      }
+
+      componentDidMount() {
+        const temp = localStorage.getItem("todos")
+        const loadedTodos = JSON.parse(temp)
+        if (loadedTodos) {
+          this.setState({
+            todos: loadedTodos
+          })
+        }
+      }
+
+      componentWillUnmount() {
+        console.log("Cleaning up...")
+      }
 
     delTodo = id => {
         this.setState({
